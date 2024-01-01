f1447b6aa9a917e9bc99333cfe2fab44ae2bf72a
Vladislav Shatilo <vladshatl123@gmail.com>
2024-01-01 14:30:20 +0300

    change methods in Manager

--- src/main/java/org/example/Manager.java
+++ src/main/java/org/example/Manager.java
@@ -16,115 +16,244 @@ public class Manager {
 
     public  void callFunctionInput()
     {
-
         file.nameInputFile += "." + file.typeInputFile;
 
+        if(!file.isArchiveInputFile && file.isEncryptInputFile)
+        {
+            switch (file.typeInputFile)
+            {
+                case "txt"->{
+
+                }
+                case "json"->{
+
+                }
+                case "xml"->{
+
+                }
+                default -> {
+
+                }
+
+            }
+
+        }
+        else if(file.isArchiveInputFile && !file.isEncryptInputFile)
+        {
+            switch (file.typeInputFile)
+            {
+                case "txt"->{
+
+                }
+                case "json"->{
+
+                }
+                case "xml"->{
+
+                }
+                default -> {
+
+                }
+
+            }
+
+        }
+        else if(file.isArchiveInputFile && file.ArchiveThanEncryptInput)
+        {
+            switch (file.typeInputFile)
+            {
+                case "txt"->{
+
+                }
+                case "json"->{
+
+                }
+                case "xml"->{
+
+                }
+                default -> {
+
+                }
+
+            }
+
+        }
+        else if(file.isArchiveInputFile && file.encryptThanArchiveInput)
+        {
+            switch (file.typeInputFile)
+            {
+                case "txt"->{
+
+                }
+                case "json"->{
+
+                }
+                case "xml"->{
+
+                }
+                default -> {
+
+                }
+
+            }
+
+        }
+        else {
+            switch (file.typeInputFile)
+            {
+                case "txt" -> {
+
+                    PlainTextProcess txtWork = new PlainTextProcess();
+                    try {
+                            Vector<String> data = txtWork.readFromPlainTextFile(file.nameInputFile);
+                            result = calculator.calculateSimple(data);
 
 
-        switch (file.typeInputFile) {
-            case "txt": {
-                PlainTextProcess txtWork = new PlainTextProcess();
-                try {
-                    if(!file.isEncryptInputFile){
-                        Vector<String> data = txtWork.readFromPlainTextFile(file.nameInputFile);
-                        result = calculator.calculateSimple(data);}
-                    else {
-                        EncryptionWork encryptionWork =new EncryptionWork();
-                        encryptionWork.decrypt(file.nameInputFile);
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+                case "json" -> {
+                    JSONWork jsonWork = new JSONWork();
+                    try {
+
+                        Vector<String> data = jsonWork.readFromJSON(file.nameInputFile);
+                        result = calculator.calculateSimple(data);
 
 
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
                     }
 
-
-                } catch (Exception e) {
-                    throw new RuntimeException(e);
                 }
-                break;
-            }
-            case "json": {
-                JSONWork jsonWork = new JSONWork();
-                try {
-
-                    Vector<String> data = jsonWork.readFromJSON(file.nameInputFile);
-                    result = calculator.calculateSimple(data);
-
-
-                } catch (Exception e) {
-                    throw new RuntimeException(e);
-                }
-                break;
-
-            }
-
-            case "xml": {
-                XMLWork xmlWork = new XMLWork();
-                try {
+                case "xml" -> {
+                    XMLWork xmlWork = new XMLWork();
+                    try {
                         Vector<String> data = xmlWork.readFromXML(file.nameInputFile);
                         result = calculator.calculateSimple(data);
 
 
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
 
-                } catch (Exception e) {
-                    throw new RuntimeException(e);
                 }
-                break;
 
             }
-            case "zip":{
-                ZipWork zipWork = new ZipWork();
-                zipWork.readZipTXT( file.nameInputFile);
-            }
-
-
-
 
         }
+
+
     }
 
 
     public void callFunctionOutput() {
 
         file.nameOutputFile += "." + file.typeOutputFile;
-        switch (file.typeOutputFile) {
-            case "txt" -> {
+        if(file.isEncryptOutputFile && !file.isArchiveOutputFile)
+        {
+            switch (file.typeOutputFile) {
+                case "txt" -> {
+
+                }
+                case "json" -> {
+
+                }
+                case "xml" -> {
+
+                }
+                default -> {
+
+                }
+            }
+
+        }
+
+        else if(!file.isEncryptOutputFile && file.isArchiveOutputFile)
+        {
+            switch (file.typeOutputFile) {
+                case "txt" -> {
+
+                }
+                case "json" -> {
+
+                }
+                case "xml" -> {
+
+                }
+                default -> {
+
+                }
+            }
+        }
+        else if(file.isEncryptOutputFile  && file.ArchiveThanEncryptOutput)
+        {
+            switch (file.typeOutputFile) {
+                case "txt" -> {
+
+                }
+                case "json" -> {
+
+                }
+                case "xml" -> {
+
+                }
+                default -> {
+
+                }
+            }
+        }
+        else if(file.isEncryptOutputFile && file.encryptThanArchiveOutput)
+        {
+            switch (file.typeOutputFile) {
+                case "txt" -> {
+
+                }
+                case "json" -> {
+
+                }
+                case "xml" -> {
+
+                }
+                default -> {
+
+                }
+            }
+        }
+        else {
+            switch (file.typeOutputFile) {
+                case "txt" -> {
 
 
-                PlainTextProcess txtWork = new PlainTextProcess();
-                try {
-                    txtWork.writeInPlainText(result, file.nameOutputFile);
-                } catch (Exception e) {
-                    throw new RuntimeException(e);
+                    PlainTextProcess txtWork = new PlainTextProcess();
+                    try {
+                        txtWork.writeInPlainText(result, file.nameOutputFile);
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+
+                }
+                case "json" -> {
+                    JSONWork jsonWork = new JSONWork();
+                    try {
+                        jsonWork.writeInJSON(result, file.nameOutputFile);
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+
+                }
+                case "xml" -> {
+                    XMLWork xmlWork = new XMLWork();
+                    try {
+                        xmlWork.writeInXml(result, file.nameOutputFile);
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+
                 }
 
             }
-            case "json" -> {
-                JSONWork jsonWork = new JSONWork();
-                try {
-                    jsonWork.writeInJSON(result, file.nameOutputFile);
-                } catch (Exception e) {
-                    throw new RuntimeException(e);
-                }
 
-            }
-            case "xml" -> {
-                XMLWork xmlWork = new XMLWork();
-                try {
-                    xmlWork.writeInXml(result, file.nameOutputFile);
-                } catch (Exception e) {
-                    throw new RuntimeException(e);
-                }
-
-            }
-            case "enc" -> {
-                Encryption encryption = new Encryption();
-                File inputFile = new File(file.nameInputFile);
-                File outputFile = new File(file.nameOutputFile);
-                try {
-                    encryption.encryptFile(inputFile, outputFile);
-                } catch (Exception exception) {
-                    System.out.println("Error encrypt file");
-                }
-            }
         }
     }
 
