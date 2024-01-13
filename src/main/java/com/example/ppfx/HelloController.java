01d04457b9fc30b73c8d186111fa6363e85371e7
Vladislav Shatilo <vladshatl123@gmail.com>
2024-01-13 18:54:36 +0300

    FX add choose rar or zip

--- src/main/java/com/example/ppfx/HelloController.java
+++ src/main/java/com/example/ppfx/HelloController.java
@@ -6,6 +6,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.ToggleGroup;
@@ -14,17 +15,125 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class HelloController {
+
+    private static final FileInformationStructure file = new FileInformationStructure();
+
+    @FXML
+    private void newArchiveInpWindow() throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("archiveInput.fxml"));
+        Parent root = loader.load();
+
+        Stage secondStage = new Stage();
+        secondStage.setTitle("Choose calculation");
+
+        secondStage.setScene(new Scene(root));
+
+        secondStage.show();
+
+    }
+
+    @FXML
+    private void newArchiveOutWindow() throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("archiveOutput.fxml"));
+        Parent root = loader.load();
+
+        Stage secondStage = new Stage();
+        secondStage.setTitle("Choose calculation");
+
+        secondStage.setScene(new Scene(root));
+
+        secondStage.show();
+
+    }
+
+
     @FXML
     private void newCalculateWindow() throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("calculate.fxml"));
         Parent root = loader.load();
 
         Stage secondStage = new Stage();
-       // secondStage.setTitle("Второе окно");
+        secondStage.setTitle("Choose calculation");
 
         secondStage.setScene(new Scene(root));
 
         secondStage.show();
+
+    }
+
+
+
+    @FXML
+    public void zipInp(ActionEvent actionEvent)
+    {
+        file.typeOfInputArchive = "zip";
+
+            try {
+                if(file.isArchiveOutputFile)
+                {
+                    newArchiveOutWindow();
+                }
+                else{
+                    newCalculateWindow();
+                }
+                Stage currentStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+                currentStage.close();
+            } catch (Exception e) {
+
+                System.out.println("Error new window" + e.toString());
+            }
+
+    }
+
+    @FXML
+    public void rarInp(ActionEvent actionEvent)
+    {
+        file.typeOfInputArchive = "rar";
+        try {
+            if(file.isArchiveOutputFile)
+            {
+                newArchiveOutWindow();
+            }
+            else{
+                newCalculateWindow();
+            }
+            Stage currentStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+            currentStage.close();
+        }
+        catch (Exception e) {
+
+            System.out.println("Error new window" + e.toString());
+        }
+    }
+
+    @FXML
+    public void zipOut(ActionEvent actionEvent)
+    {
+        file.typeOfOutputArchive = "zip";
+        try {
+            newCalculateWindow();
+            Stage currentStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+            currentStage.close();
+        }
+        catch (Exception e) {
+
+            System.out.println("Error new window" + e.toString());
+        }
+    }
+
+    @FXML
+    public void rarOut(ActionEvent actionEvent)
+    {
+        file.typeOfOutputArchive = "rar";
+        try {
+            newCalculateWindow();
+            Stage currentStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+            currentStage.close();
+        }
+        catch (Exception e) {
+
+            System.out.println("Error new window" + e.toString());
+        }
 
     }
 
@@ -41,26 +150,90 @@ public class HelloController {
     private ToggleGroup typeOutputToggleGroup;
 
     @FXML
-    public void getData(ActionEvent actionEvent) {
+    public void getData(ActionEvent actionEvent) throws IOException {
         RadioButton selectedInputType = (RadioButton) extensionInputToggleGroup.getSelectedToggle();
-        System.out.println(selectedInputType.getText());
+        file.typeInputFile = selectedInputType.getText();
+
 
         selectedInputType = (RadioButton) extensionOutputToggleGroup.getSelectedToggle();
-        System.out.println(selectedInputType.getText());
+        file.typeOutputFile = selectedInputType.getText();
 
         selectedInputType = (RadioButton) typeInputToggleGroup.getSelectedToggle();
-        System.out.println(selectedInputType.getText());
+        switch (selectedInputType.getText()) {
+            case "Just file"-> {
+                file.isArchiveInputFile = false;
+                file.isEncryptInputFile = false;
+            }
+            case "Encrypt file" -> {
+
+                file.isArchiveInputFile = false;
+                file.isEncryptInputFile = true;
+            }
+            case "Archive file" -> {
+                file.isArchiveInputFile = true;
+                file.isEncryptInputFile = false;
+            }
+            case "Encrypt -> Archive file"-> {
+                file.isArchiveInputFile = true;
+                file.isEncryptInputFile = true;
+                file.encryptThanArchiveInput =true;
+            }
+            case "Archive -> Encrypt file" ->{
+                file.isArchiveInputFile = true;
+                file.isEncryptInputFile = true;
+                file.ArchiveThanEncryptInput =true;
+            }
+            default -> {
+            }
+        }
 
         selectedInputType = (RadioButton) typeOutputToggleGroup.getSelectedToggle();
-        System.out.println(selectedInputType.getText());
-        try {
-            newCalculateWindow();
-            Stage currentStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
-            currentStage.close();
-        }
-        catch (Exception e) {
+        switch (selectedInputType.getText()) {
+            case "Just file" -> {
+                file.isArchiveOutputFile = false;
+                file.isEncryptOutputFile = false;
 
-            System.out.println("Error new window" + e.toString());
+            }
+            case "Encrypt file" -> {
+                file.isArchiveOutputFile = false;
+                file.isEncryptOutputFile = true;
+            }
+            case "Archive file" -> {
+                file.isArchiveOutputFile = true;
+                file.isEncryptOutputFile = false;
+            }
+            case "Encrypt -> Archive file"-> {
+                file.isArchiveOutputFile = true;
+                file.isEncryptOutputFile= true;
+                file.encryptThanArchiveOutput= true;
+
+
+            }
+            case "Archive -> Encrypt file"->{
+                file.isArchiveOutputFile = true;
+                file.isEncryptOutputFile= true;
+                file.ArchiveThanEncryptOutput =true;
+
+            }
+            default -> {
+            }
         }
+        Stage currentStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        currentStage.close();
+
+        if(file.isArchiveInputFile) {
+            newArchiveInpWindow();
+        }
+        else if(file.isArchiveOutputFile)
+        {
+            newArchiveOutWindow();
+        }
+        else{
+            newCalculateWindow();
+        }
+
+     
+
+
     }
 }
